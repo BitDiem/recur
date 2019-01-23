99fa3eae49fb97a6f5b3dd45dc1eca6e565e8875
Arash Emami <aemami99@gmail.com>
2019-01-22 22:36:54 -0800

    Removed logic around the Balance contract as it was redundant

--- contracts/StandardSubscription.sol
+++ contracts/StandardSubscription.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.0;
 
 import "./payment/PaymentProcessor.sol";
-import "./payment/Balance.sol";
 import "./terms/IPaymentTerms.sol";
 import "./accounts/IAuthorizedTokenTransferer.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
@@ -10,7 +9,6 @@ contract StandardSubscription is PaymentProcessor {
     
     using SafeMath for uint;
 
-    Balance private _debt;
     IPaymentTerms private _paymentTerms;
 
     event SubscriptionEnded(address endedBy);
@@ -25,18 +23,13 @@ contract StandardSubscription is PaymentProcessor {
         PaymentProcessor(payor, payee, authorizedTransferer, token)
         public 
     {
-        _debt = new Balance();
         _paymentTerms = paymentTerms;
     }
 
     function payCurrentAmountDue() public {
         uint newAmountDue = _paymentTerms.currentAmountDue();
-        uint totalAmountDue = newAmountDue.add(_debt.get());
-        (, uint remainder) = pay(totalAmountDue);
-
-        _debt.set(remainder);
-
-        _paymentTerms.markAsPaid(newAmountDue);
+        (uint amountPaid,) = pay(newAmountDue);
+        _paymentTerms.markAsPaid(amountPaid);
     }
 
     function endSubscription() public {
