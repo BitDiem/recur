662375ec45107158c72a96aa18059ab87de886ce
Arash Emami <aemami99@gmail.com>
2019-02-14 01:46:11 -0800

    Broke payment credit logic into its own contract (properly this time)

---
+++ contracts/payment/PaymentCredit.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+
+import "../payment/roles/CreditAdminRole.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+/**
+ * @title PaymentCredit
+ * @dev Encapsulates an amount of credit that can be debited against when making a payment
+ */
+contract PaymentCredit is CreditAdminRole {
+
+    using SafeMath for uint;
+
+    uint private _credit;
+
+    event CreditChanged(uint creditTotal);
+
+    function getCredit() public view returns (uint) {
+        return _credit;
+    }
+
+    function addCredit(uint amount) public onlyCreditAdmin {
+        require(amount > 0);
+        _setCredit(_credit.add(amount));
+    }
+
+    function removeCredit(uint amount) public onlyCreditAdmin {
+        require(amount > 0);
+        _setCredit(_credit.sub(amount));
+    }
+
+    function _setCredit(uint credit) internal {
+        _credit = credit;
+        emit CreditChanged(_credit);
+    }
+
+}
