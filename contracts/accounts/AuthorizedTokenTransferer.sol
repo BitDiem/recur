47ccf5b7ba5159484ff2d186ae7dce35b3582080
Arash Emami <aemami99@gmail.com>
2019-02-19 15:38:57 -0800

    Updated authorized token transferer contract to utilize open zeppelin's whitelistedrole (instead of ownable)

--- contracts/accounts/AuthorizedTokenTransferer.sol
+++ contracts/accounts/AuthorizedTokenTransferer.sol
@@ -1,18 +1,17 @@
 pragma solidity ^0.5.0;
 
 import "../accounts/IAuthorizedTokenTransferer.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/access/roles/WhitelistedRole.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
-contract AuthorizedTokenTransferer is IAuthorizedTokenTransferer, Ownable { 
-    // TODO: this might need to be a whitelistedadmin type implementation - otherwise only one frontend could add at a time
+contract AuthorizedTokenTransferer is IAuthorizedTokenTransferer, WhitelistedRole { 
 
-    mapping(address => bool) _whitelistedCallers;
-
-    modifier onlyWhitelistedCallers {
-        require(_whitelistedCallers[msg.sender]);
-        _;
-    }
+    event TokenTransferred(
+        address from, 
+        address to, 
+        address token, 
+        uint amount
+    );
 
     function transfer(
         address from, 
@@ -21,19 +20,16 @@ contract AuthorizedTokenTransferer is IAuthorizedTokenTransferer, Ownable {
         uint amount
     ) 
         public
-        onlyWhitelistedCallers
+        onlyWhitelisted
     {
+        /*require(from != address(0));
+        require(to != address(0));
+        require(token != address(0));
+        require(amount > 0);*/
         IERC20 tokenContract = IERC20(token);
         require (tokenContract.allowance(from, address(this)) >= amount, "Token transfer not authorized");
         tokenContract.transferFrom(from, to, amount);
-    }
-
-    function addToWhitelist(address account) public onlyOwner {
-        _whitelistedCallers[account] = true;
-    }
-
-    function removeFromWhitelist(address account) public onlyOwner {
-        _whitelistedCallers[account] = false;
+        emit TokenTransferred(from, to, token, amount);
     }
 
 }
