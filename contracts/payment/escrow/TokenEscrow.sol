3ed5848dcbf040dba3d0d85a1a41d33155e8e0d1
Arash Emami <aemami99@gmail.com>
2019-02-26 17:24:55 -0800

    cleanup of TokenEscrow contract

--- contracts/payment/escrow/TokenEscrow.sol
+++ contracts/payment/escrow/TokenEscrow.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.0;
 
 import "../../payment/roles/TokenWithdrawerRole.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 /**
@@ -10,20 +9,18 @@ import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
  */
 contract TokenEscrow is TokenWithdrawerRole {
 
-    event TokensWithdrawn(address withdrawer, IERC20 token, uint amount);
+    event TokenWithdrawn(address indexed withdrawer, IERC20 indexed token, uint amount);
 
     /**
      * @dev Allows allows an approved token withdrawer address to withdraw any erc20 token held in this contract.
      * @param token The address of the ERC20 token to withdraw
      * @param amount The balance amount to withdraw
      */
-    function withdrawToken(address token, uint amount) public onlyTokenWithdrawer {
+    function withdrawToken(IERC20 token, uint amount) public onlyTokenWithdrawer {
         require(amount > 0);
-        require(token != address(0));
         address to = msg.sender;
-        IERC20 tokenContract = IERC20(token);
-        tokenContract.transfer(to, amount);
-        emit TokensWithdrawn(to, tokenContract, amount);
+        token.transfer(to, amount);
+        emit TokenWithdrawn(to, token, amount);
     }
 
 }
