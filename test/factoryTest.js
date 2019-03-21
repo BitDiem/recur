980a77cd4d806b5db7be0aba885eee675993a7f5
Arash Emami <aemami99@gmail.com>
2019-03-21 16:31:48 -0700

    added tests for fixed date creations

--- test/factoryTest.js
+++ test/factoryTest.js
@@ -7,36 +7,36 @@ const assert = require('assert')
 
 contract("Publisher FrontEnd Test", accounts => {
 
-    let tokenBank;
-    let payor;
-    let payee;
-    let mockERC20;
-    let authorizedTokenTransferer;
-    let subscriptionFrontEnd;
-    let transaction;
-    let log;
+  let tokenBank;
+  let payor;
+  let payee;
+  let mockERC20;
+  let authorizedTokenTransferer;
+  let subscriptionFrontEnd;
+  let transaction;
+  let log;
 
-    const startingTokenBalance = 100;
+  const startingTokenBalance = 100;
 
-    beforeEach(async function() {   
-        tokenBank = accounts[0];
-        payor = accounts[1];
-        payee = accounts[2];
+  beforeEach(async function() {   
+      tokenBank = accounts[0];
+      payor = accounts[1];
+      payee = accounts[2];
 
-        mockERC20 = await MockERC20.new("Mock ERC20", "MERC20", tokenBank, startingTokenBalance * 2);
-        await mockERC20.transfer(payor, startingTokenBalance, {from: tokenBank});
+      mockERC20 = await MockERC20.new("Mock ERC20", "MERC20", tokenBank, startingTokenBalance * 2);
+      await mockERC20.transfer(payor, startingTokenBalance, {from: tokenBank});
 
-        publisherFrontEnd = await PublisherFrontEnd.new();
+      publisherFrontEnd = await PublisherFrontEnd.new();
 
-        transaction = await publisherFrontEnd.createPublisher();
-        log = transaction.logs[0];
-        let publisherAddress = log.args.publisher;
-        subscriptionFrontEnd = await SubscriptionFrontEnd.at(publisherAddress);
+      transaction = await publisherFrontEnd.createPublisher();
+      log = transaction.logs[0];
+      let publisherAddress = log.args.publisher;
+      subscriptionFrontEnd = await SubscriptionFrontEnd.at(publisherAddress);
 
-        let authorizedTokenTransfererAddress = (await subscriptionFrontEnd.getTokenTransferer()).valueOf();
-        authorizedTokenTransferer = await AuthorizedTokenTransferer.at(authorizedTokenTransfererAddress);
-        await mockERC20.approve(authorizedTokenTransferer.address, 10000000, {from: payor});
-    })
+      let authorizedTokenTransfererAddress = (await subscriptionFrontEnd.getTokenTransferer()).valueOf();
+      authorizedTokenTransferer = await AuthorizedTokenTransferer.at(authorizedTokenTransfererAddress);
+      await mockERC20.approve(authorizedTokenTransferer.address, 10000000, {from: payor});
+  })
 
   it("should create a fixed interval subscription", async () => {  
     transaction = await subscriptionFrontEnd.createFixedIntervalSubscription(
@@ -52,15 +52,12 @@ contract("Publisher FrontEnd Test", accounts => {
     subscription = await StandardSubscription.at(subscriptionAddress);
   });
 
-  /*
-  it("should create a monthly subscription", async () => {    
+  it("should create monthly subscription", async () => {  
     transaction = await subscriptionFrontEnd.createMonthlySubscription(
         payee,
         mockERC20.address,
         1,
-        2019,
-        1,
-        1,
+        2019, 3, 21,
         {from: payor}
     );
     log = transaction.logs[0];
@@ -68,14 +65,12 @@ contract("Publisher FrontEnd Test", accounts => {
     subscription = await StandardSubscription.at(subscriptionAddress);
   });
 
-  it("should create a quarterly subscription", async () => {    
+  it("should create multi monthly subscription", async () => {  
     transaction = await subscriptionFrontEnd.createMultiMonthlySubscription(
         payee,
         mockERC20.address,
         1,
-        2019,
-        1,
-        1,
+        2019, 3, 21,
         3,
         {from: payor}
     );
@@ -84,19 +79,17 @@ contract("Publisher FrontEnd Test", accounts => {
     subscription = await StandardSubscription.at(subscriptionAddress);
   });
 
-  it("should create a yearly subscription", async () => {    
+  it("should create yearly subscription", async () => {  
     transaction = await subscriptionFrontEnd.createYearlySubscription(
         payee,
         mockERC20.address,
         1,
-        2019,
-        1,
-        1,
+        2019, 3, 21,
         {from: payor}
     );
     log = transaction.logs[0];
     let subscriptionAddress = log.args.subscriptionAddress;
     subscription = await StandardSubscription.at(subscriptionAddress);
-  });*/
+  });
 
 });
