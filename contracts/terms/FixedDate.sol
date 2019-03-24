365e5bf9a364d9d6bfcc24b8e17d56aa60fd9fbd
Arash Emami <aemami99@gmail.com>
2019-03-24 00:20:44 -0700

    Refactored FixedDate to use the DateTime library

--- contracts/terms/FixedDate.sol
+++ contracts/terms/FixedDate.sol
@@ -1,25 +1,28 @@
 pragma solidity ^0.5.0;
 
 import "../payment/PaymentObligation.sol";
-import "../lib/date/BokkyPooBahsDateTimeLibrary.sol";
+import "../lib/date/DateTime.sol";
+import "../lib/date/MockableCurrentTime.sol";
 
 /**
  * @title FixedDate
  * @dev Base contract for specifying an exact datetime that payment is due on.  
- * Year, month, and day of next payment can be manipulated by child contracts.
+ * Year and month of next payment can be manipulated by child contracts.
  * Contract stores the year, month, day and total seconds offset, in addition to a timestamp value.
  * Justification: saves a call to BokkyPooBahsDateTimeLibrary._daysToDate, 
- * at the expense of using a bit more storage (4 additional vs. just storing a timestamp)
+ * at the expense of using a bit more storage (4 additional uint's vs. just storing a timestamp).
+ * NOTE: For this contract as well as those deriving from it, specifying a @nextPaymentDay greater than 31 will ensure 
+ * that payment is due on the last day of the month, regardless of how many days are in that month.
  */
-contract FixedDate is PaymentObligation {
+contract FixedDate is PaymentObligation, MockableCurrentTime {
 
     uint constant SECONDS_PER_HOUR = 60 * 60;
     uint constant SECONDS_PER_MINUTE = 60;
 
     uint internal _nextPaymentYear;
     uint internal _nextPaymentMonth;
-    uint internal _nextPaymentDay;
 
+    uint private _nextPaymentDay;
     uint private _secondsOffset;
     uint private _nextPaymentTimestamp;
     uint private _amount;
@@ -38,15 +41,16 @@ contract FixedDate is PaymentObligation {
         public
     {
         require(amount > 0);
-        require(_isValidMonthAndYear(nextPaymentMonth, nextPaymentYear));
+        require(DateTime.isValidMonthAndYear(nextPaymentMonth, nextPaymentYear));
         require(nextPaymentDay > 0);
+        require(DateTime.isValidTime(nextPaymentHour, nextPaymentMinute, nextPaymentSecond));
 
         _amount = amount;
         _nextPaymentYear = nextPaymentYear;
         _nextPaymentMonth = nextPaymentMonth;
         _nextPaymentDay = nextPaymentDay;
 
-        _secondsOffset = _getTotalSeconds(nextPaymentHour, nextPaymentMinute, nextPaymentSecond);
+        _secondsOffset = DateTime.totalSeconds(nextPaymentHour, nextPaymentMinute, nextPaymentSecond);
         _calculateNextPaymentTimestamp();
     }
 
@@ -55,53 +59,24 @@ contract FixedDate is PaymentObligation {
             return 0;
 
         uint currentPaymentDue = _nextPaymentTimestamp;
-
         _advance();
-        _calculateNextPaymentTimestamp();
-        
+        _calculateNextPaymentTimestamp();    
         emit PaymentDue(currentPaymentDue, _amount);
-
         return _amount;
     }
 
-    /// Wrap the call and make it internal - makes it easy to create a derived mock class to test exact hypothetical dateTimes
-    function _getCurrentTimeInUnixSeconds() internal view returns (uint) {
-        return now;
-    }
-
+    /// override this function to change month or year of next payment due date
     function _advance() internal;
 
     /// saving the year / month / day saves a call to BokkyPooBahsDateTimeLibrary._daysToDate, 
     /// at the expense of using a bit more storage (4x or 7x vs. just storing a timestamp)
     function _calculateNextPaymentTimestamp() private {
         // adjust for the days of month for payment days greater than 28 (since all months have at least 28 days)
-        uint adjustedPaymentDay = _nextPaymentDay;
-        if (_nextPaymentDay > 28) {
-            uint daysInMonth = BokkyPooBahsDateTimeLibrary._getDaysInMonth(_nextPaymentYear, _nextPaymentMonth);
-            if (_nextPaymentDay > daysInMonth) {
-                adjustedPaymentDay = daysInMonth;
-            }
-        }
+        uint adjustedPaymentDay = DateTime.constrainToDaysInMonth(_nextPaymentYear, _nextPaymentMonth, _nextPaymentDay);
 
         // create the timestamp from year, month, and adjusted date
-        uint date = BokkyPooBahsDateTimeLibrary.timestampFromDate(
-            _nextPaymentYear,
-            _nextPaymentMonth,
-            adjustedPaymentDay
-        );
-        _nextPaymentTimestamp = date + _secondsOffset;
-    }
-
-    /// Code based on https://github.com/bokkypoobah/BokkyPooBahsDateTimeLibrary
-    function _isValidMonthAndYear(uint month, uint year) private pure returns (bool) {
-        return (year >= 1970 && month > 0 && month <= 12);
-    }
-
-    function _getTotalSeconds(uint hour, uint minute, uint second) private pure returns (uint) {
-        return 
-            hour * SECONDS_PER_HOUR + 
-            minute * SECONDS_PER_MINUTE + 
-            second;
+        uint date = DateTime.timestampFromDate(_nextPaymentYear, _nextPaymentMonth, adjustedPaymentDay);
+        _nextPaymentTimestamp = DateTime.add(date, _secondsOffset);
     }
 
 }
