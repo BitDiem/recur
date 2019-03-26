8cb982fc03df5d800431b9c31eb3a18a749873bc
Arash Emami <aemami99@gmail.com>
2019-03-25 19:03:36 -0700

    Expanded the seconds factory class to create minutes, hours, and days incremented versions of Seconds

--- contracts/lib/factory/SecondsTermsFactory.sol
+++ contracts/lib/factory/SecondsTermsFactory.sol
@@ -1,32 +1,101 @@
 pragma solidity ^0.5.0;
 
 import "../../terms/Seconds.sol";
+import "../../lib/date/DateTime.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 library SecondsTermsFactory {
 
-    function create(
+    using SafeMath for uint;
+
+    uint constant SECONDS_PER_MINUTE = 60;
+    uint constant SECONDS_PER_HOUR = 60 * 60;
+    uint constant SECONDS_PER_DAY = 24 * 60 * 60;
+
+    function createDays(
         uint amount,
-        uint nextPaymentYear,
-        uint nextPaymentMonth,
-        uint nextPaymentDay,
-        uint nextPaymentHour,
-        uint nextPaymentMinute,
-        uint nextPaymentSecond,
+        uint year,
+        uint month,
+        uint day,
+        uint hour,
+        uint minute,
+        uint second,
+        uint daysIncrement
+    )
+        external
+        returns (Seconds)
+    {
+        return _create(amount, year, month, day, hour, minute, second, daysIncrement.mul(SECONDS_PER_DAY));
+    }
+
+    function createHours(
+        uint amount,
+        uint year,
+        uint month,
+        uint day,
+        uint hour,
+        uint minute,
+        uint second,
+        uint hoursIncrement
+    )
+        external
+        returns (Seconds)
+    {
+        return _create(amount, year, month, day, hour, minute, second, hoursIncrement.mul(SECONDS_PER_HOUR));
+    }
+
+    function createMinutes(
+        uint amount,
+        uint year,
+        uint month,
+        uint day,
+        uint hour,
+        uint minute,
+        uint second,
+        uint minutesIncrement
+    )
+        external
+        returns (Seconds)
+    {
+        return _create(amount, year, month, day, hour, minute, second, minutesIncrement.mul(SECONDS_PER_MINUTE));
+    }
+
+    function createSeconds(
+        uint amount,
+        uint year,
+        uint month,
+        uint day,
+        uint hour,
+        uint minute,
+        uint second,
         uint secondsIncrement
     )
         external
         returns (Seconds)
     {
-        return new Seconds(
-            amount, 
-            nextPaymentYear, 
-            nextPaymentMonth, 
-            nextPaymentDay, 
-            nextPaymentHour, 
-            nextPaymentMinute, 
-            nextPaymentSecond,
-            secondsIncrement
-        );
+        return _create(amount, year, month, day, hour, minute, second, secondsIncrement);
+    }
+
+    function createSecondsFromTimestamp(uint amount, uint firstPaymentDueTimestamp, uint secondsIncrement) external returns (Seconds) {
+        return new Seconds(amount, firstPaymentDueTimestamp, secondsIncrement);
+    }
+
+    function _create(
+        uint amount,
+        uint year,
+        uint month,
+        uint day,
+        uint hour,
+        uint minute,
+        uint second,
+        uint secondsIncrement
+    )
+        private
+        returns (Seconds)
+    {
+        require(DateTime.isValidDateTime(year, month, day, hour, minute, second));
+        uint firstPaymentDueTimestamp = DateTime.timestampFromDateTime(year, month, day, hour, minute, second);
+        return new Seconds(amount, firstPaymentDueTimestamp, secondsIncrement);
     }
 
 }
